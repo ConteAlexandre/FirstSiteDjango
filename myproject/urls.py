dd687ece093248c23aeea7ca623c068e638863da
ConteAlexandre <pro.alexandre.conte@gmail.com>
2020-02-28 15:29:18 +0100

    Start a new app and define a first model in the file models.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     # Here, we define the waves who content the pattern admin in the url are contained in only line
-    url(r'^admin/', admin.site.urls)
+    url(r'^admin/', admin.site.urls),
+    url(r'', include('main.urls')),
 ]
