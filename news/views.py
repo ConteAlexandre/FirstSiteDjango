d02739656f991edb7e3a085102b1bdf2c5d90d73
ConteAlexandre <pro.alexandre.conte@gmail.com>
2020-03-23 11:47:32 +0100

    We have a method for add the news with the panel and method for validation form with input empty

--- news/views.py
+++ news/views.py
@@ -30,14 +30,32 @@ def news_add(request):
 
     # Condition for add the different input in the database, if this method equal as post
     if request.method == "POST":
+
         # Title
         newstitle = request.POST.get('newstitle')
+
         # Category
         newscat = request.POST.get('newscat')
+
         # Short text
         newstxtshort = request.POST.get("newstxtshort")
+
         # Body text
         newstxt = request.POST.get("newstxt")
+
+        # Validation Form
+        if newstitle == "" or newstxt == "" or newstxtshort == "" or newscat == "":
+            error = "All Fields Required"
+            return render(request, 'back/error.html', { 'error': error })
+
+        # Here, we define the new object News and add the variables
+        b = News(name=newstitle, short_txt=newstxtshort, body_txt=newstxt, date="2020", pic="-", writer="-", catname=newscat, catid=0, show=0)
+
+        # We call the method for recover the object
+        b.save()
+
+        # And redirect to the panel list news
+        return redirect('news_list')
 
     # Define a new page for add a new
     return render(request, 'back/news_add.html')
